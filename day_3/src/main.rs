7c582e91296bafe718decc2e14aaf47bd07f63d0
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-12-05 12:36:21 +0000

    WIP attempt at Part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,4 +1,4 @@
-use ndarray::Array2;
+use ndarray::{s, Array, Array2};
 
 fn main() {
     let input_data = read_input();
@@ -12,6 +12,30 @@ fn part_one_solution(grid: &Array2<char>) -> u32 {
     let part_numbers = find_parts(grid);
 
     part_numbers.iter().sum()
+}
+
+fn part_two_solution(grid: &Array2<char>) -> u32 {
+    // update the core parts to find the digit that has the adjacent symbol
+    // from that position I can then move left and right to properly construct it
+
+    // iterate through, find the * symbol
+    // get all surrounding cells
+    // if at least two numbers then for all numbers get position
+    // work left and right to fully construct number
+
+    todo!()
+}
+
+fn find_gear_ratios(grid: &Array2<char>) -> Vec<u32> {
+    let mut gear_ratios: Vec<u32> = vec![];
+
+    for ((row, col), position) in grid.indexed_iter() {
+        if position == &'*' {
+            let surrounding = surrounding_cells(grid, row, col);
+        }
+    }
+
+    gear_ratios
 }
 
 fn find_parts(grid: &Array2<char>) -> Vec<u32> {
@@ -78,6 +102,37 @@ fn number_finished(grid: &Array2<char>, row: usize, col: usize) -> bool {
     }
 }
 
+fn construct_number(grid: &Array2<char>, row: usize, col: usize) -> u32 {
+    let digit = grid.get((row, col)).unwrap();
+    let mut number: Vec<&char> = vec![digit];
+
+    let left = grid.slice(s![row, 0..col]);
+    let right = grid.slice(s![row, col + 1..]);
+
+    for char in left.iter().rev() {
+        if char.is_ascii_digit() {
+            number.insert(0, char)
+        } else {
+            break;
+        }
+    }
+
+    for char in right.iter() {
+        if char.is_ascii_digit() {
+            number.push(char)
+        } else {
+            break;
+        }
+    }
+
+    number
+        .iter()
+        .cloned()
+        .collect::<String>()
+        .parse::<u32>()
+        .unwrap()
+}
+
 fn process_input(input: &str) -> Array2<char> {
     let grid = input
         .lines()
@@ -99,13 +154,28 @@ fn read_input() -> String {
 mod test_super {
     use super::*;
 
+    fn test_grid() -> Array2<char> {
+        process_input(include_str!("../data/test_input.txt"))
+    }
+
     #[test]
     fn test_find_parts() {
-        let grid = process_input(include_str!("../data/test_input.txt"));
         assert_eq!(
-            find_parts(&grid),
+            find_parts(&test_grid()),
             vec![467, 35, 633, 617, 592, 755, 664, 598]
         );
+    }
+
+    #[test]
+    fn test_construct_number() {
+        let grid = test_grid();
+        assert_eq!(construct_number(&grid, 0, 5), 114);
+        assert_eq!(construct_number(&grid, 0, 6), 114);
+        assert_eq!(construct_number(&grid, 0, 7), 114);
+
+        assert_eq!(construct_number(&grid, 4, 0), 617);
+        assert_eq!(construct_number(&grid, 4, 1), 617);
+        assert_eq!(construct_number(&grid, 4, 2), 617);
     }
 
     #[test]
