d5ad724440c23ce8c15207bf63bc5eb617913cb2
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-12-06 14:35:39 +0000

    Part One solution

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -3,19 +3,59 @@ use std::{collections::HashMap, ops::Range, str::FromStr};
 use strum_macros::EnumString;
 
 fn main() {
-    println!("Hello, world!");
-
     let text = read_input();
     let almanac = process_input(&text);
-    // write helper function when given a value will respond with the equiv
-    //      if in source range, find offset from start and apply offset to dest
-    //      if not then return same value
-    // write high level function to map from seed -> soil -> fert -> water -> light -> temp -> humidity -> location
     let part_one_answer = part_one_solution(&almanac);
+    println!("Part One Answer is {part_one_answer}");
 }
 
-fn part_one_solution(almanac: &Almanac) -> u32 {
-    todo!()
+fn part_one_solution(almanac: &Almanac) -> u64 {
+    almanac
+        .seeds
+        .iter()
+        .map(|seed| location_for_seed(almanac, seed))
+        .min()
+        .unwrap()
+}
+
+fn location_for_seed(almanac: &Almanac, seed: &u64) -> u64 {
+    let soil_dest = almanac
+        .maps
+        .get(&MapType::SeedtoSoil)
+        .unwrap()
+        .destination_for(seed);
+    let fertilizer_dest = almanac
+        .maps
+        .get(&MapType::SoiltoFertilizer)
+        .unwrap()
+        .destination_for(&soil_dest);
+    let water_dest = almanac
+        .maps
+        .get(&MapType::FertilizertoWater)
+        .unwrap()
+        .destination_for(&fertilizer_dest);
+    let light_dest = almanac
+        .maps
+        .get(&MapType::WatertoLight)
+        .unwrap()
+        .destination_for(&water_dest);
+    let temperature_dest = almanac
+        .maps
+        .get(&MapType::LighttoTemperature)
+        .unwrap()
+        .destination_for(&light_dest);
+    let humidity_dest = almanac
+        .maps
+        .get(&MapType::TemperaturetoHumidity)
+        .unwrap()
+        .destination_for(&temperature_dest);
+    let location_dest = almanac
+        .maps
+        .get(&MapType::HumiditytoLocation)
+        .unwrap()
+        .destination_for(&humidity_dest);
+
+    location_dest
 }
 
 fn read_input() -> String {
@@ -25,9 +65,9 @@ fn read_input() -> String {
 fn process_input(text: &str) -> Almanac {
     let parts: Vec<&str> = text.split("\n\n").collect();
     // seeds
-    let seeds: Vec<u32> = parts[0].split(' ').collect::<Vec<&str>>()[1..]
+    let seeds: Vec<u64> = parts[0].split(' ').collect::<Vec<&str>>()[1..]
         .iter()
-        .map(|seed| seed.parse::<u32>().unwrap())
+        .map(|seed| seed.parse::<u64>().unwrap())
         .collect();
 
     // maps
@@ -40,9 +80,9 @@ fn process_input(text: &str) -> Almanac {
             let map_ranges: Vec<MapRange> = map_lines[1..]
                 .iter()
                 .map(|range_line| {
-                    let range_parts: Vec<u32> = range_line
+                    let range_parts: Vec<u64> = range_line
                         .split(' ')
-                        .map(|range_part| range_part.parse::<u32>().unwrap())
+                        .map(|range_part| range_part.parse::<u64>().unwrap())
                         .collect();
                     MapRange::new(range_parts[0], range_parts[1], range_parts[2])
                 })
@@ -76,7 +116,7 @@ enum MapType {
     HumiditytoLocation,
 }
 struct Almanac {
-    seeds: Vec<u32>,
+    seeds: Vec<u64>,
     maps: HashMap<MapType, Map>,
 }
 
@@ -84,16 +124,33 @@ struct Map {
     ranges: Vec<MapRange>,
 }
 
+impl Map {
+    fn destination_for(&self, source: &u64) -> u64 {
+        let map_range = self
+            .ranges
+            .iter()
+            .find(|map_range| map_range.source_range.contains(&source));
+
+        match map_range {
+            Some(map_range) => {
+                let diff = source - map_range.source_range.start;
+                map_range.destination_range.start + diff
+            }
+            None => *source, // if no mapping then its the same value in the destination
+        }
+    }
+}
+
 struct MapRange {
-    dest_range_start: u32,
-    source_range_start: u32,
-    range_length: u32,
-    destination_range: Range<u32>,
-    source_range: Range<u32>,
+    dest_range_start: u64,
+    source_range_start: u64,
+    range_length: u64,
+    destination_range: Range<u64>,
+    source_range: Range<u64>,
 }
 
 impl MapRange {
-    fn new(dest: u32, source: u32, length: u32) -> Self {
+    fn new(dest: u64, source: u64, length: u64) -> Self {
         let destination_range = dest..(dest + length);
         let source_range = source..(source + length);
 
@@ -137,5 +194,51 @@ mod test_super {
         assert_eq!(soil_map.ranges[0].range_length, 2);
         assert_eq!(soil_map.ranges[0].destination_range, (50..52));
         assert_eq!(soil_map.ranges[0].source_range, (98..100));
+    }
+
+    #[test]
+    fn test_destination_for_in_range() {
+        let almanac = test_data();
+        let seed_to_soil_map = almanac.maps.get(&MapType::SeedtoSoil).unwrap();
+        let soil_number = seed_to_soil_map.destination_for(&53);
+
+        assert_eq!(soil_number, 55);
+    }
+
+    #[test]
+    fn test_destination_for_out_of_range() {
+        let almanac = test_data();
+        let seed_to_soil_map = almanac.maps.get(&MapType::SeedtoSoil).unwrap();
+        let soil_number = seed_to_soil_map.destination_for(&10);
+
+        assert_eq!(soil_number, 10);
+    }
+
+    #[test]
+    fn test_location_for_seed() {
+        let almanac = test_data();
+
+        assert_eq!(location_for_seed(&almanac, &79), 82);
+        assert_eq!(location_for_seed(&almanac, &14), 43);
+        assert_eq!(location_for_seed(&almanac, &55), 86);
+        assert_eq!(location_for_seed(&almanac, &13), 35);
+    }
+
+    #[test]
+    fn test_part_one_solution() {
+        let almanac = test_data();
+        let part_one_answer = part_one_solution(&almanac);
+
+        assert_eq!(part_one_answer, 35);
+    }
+
+    #[test]
+    fn test_part_one_real_answer() {
+        let input = String::from(include_str!("../data/puzzle_input.txt"));
+        let almanac = process_input(&input);
+
+        let answer = part_one_solution(&almanac);
+
+        assert_eq!(answer, 806029445);
     }
 }
