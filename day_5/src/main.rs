f61ae64e39a1937dd9a8cfdb76ad125de0e28265
Tim Hoiberg <tim.hoiberg@gmail.com>
2023-12-06 13:02:57 +0000

    Add initial structure for day 5

---
+++ day_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+
+    // split on \n\n
+    // first section is initial seeds
+    // every other section is 0th line, name,
+    //      every other line is a range
+    // write helper function when given a value will respond with the equiv
+    //      if in source range, find offset from start and apply offset to dest
+    //      if not then return same value
+    // write high level function to map from seed -> soil -> fert -> water -> light -> temp -> humidity -> location
+}
